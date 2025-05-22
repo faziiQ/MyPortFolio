a9558e5dc23087720038368096449106251f0724
faziiQ <YourEmail@example.comfm013361@gmail.com>
2025-05-22 14:47:49 +0500

    Add package-lock.json

--- OneDrive/Desktop/React JS/Portfolio/myportfolio/vite.config.js
+++ OneDrive/Desktop/React JS/Portfolio/myportfolio/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
+  plugins: [
+    tailwindcss(),
+    react()],
+    build: {
     rollupOptions: {
       external: [
         'react-icons/md',
-        'react-icons/im'
+        'react-icons/im',
+         'react-icons/fa'  
+
       ]
     }
-  }
+  },
 })
+
+
